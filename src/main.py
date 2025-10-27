ec633d14f8621faf9bb3e09e5697127c730ef0c3
0xbea <19748276+0xbea@users.noreply.github.com>
2025-10-27 17:36:49 +0100

    bk

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+def main():
+    new = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(new)
+
+
+if __name__ == "__main__":
+    main()
